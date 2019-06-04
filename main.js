c236d5e94ea959a6b384a92c0d2aedf0111debb8
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-06-04 08:19:07 -0700

    commits

---
+++ main.js
@@ -0,0 +1,32 @@
+import { Tamagatchi } from './../src/tamagatchi.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+$(document).ready(function()){
+  $("#name").submit(function(event){
+    event.preventDefault();
+    $("#tamoStats").show();
+
+  let name = $("#tamoName").val();
+  let tamo = new Tamagatchi(name);
+  $("#satiateLevel").text(tamo.satiateLevel);
+  $("#restLevel").text(tamo.restLevel);
+  $("#happyLevel").text(tamo.happyLevel);
+  tamo.gettingHungry();
+  tamo.getSleepy();
+  tamo.happyDown();
+  $(".showName").text(tamo.name);
+
+  $("#feed").click(function(){
+    tamo.feed();
+  })
+  $("#pet").click(function(){
+    tamo.pet();
+  })
+  $("#exercise").click(function(){
+    tamo.exercise();
+  })
+
+  })
+}
---
+++ tamagatchi.js
@@ -0,0 +1,58 @@
+export class Tamagatchi {
+    constructor(name){
+        this.name = name;
+        this.satiateLevel = 10;
+        this.happyLevel = 10;
+        this.restLevel  = 10;
+    }
+
+    gettingHungry() {
+        setInterval(() => {
+         this.satiateLevel--;
+        }, 1000);
+    }
+
+     screams() {
+        if (this.satiateLevel > 0) {
+            return false;
+        } else {
+            return alert(`Feed me NOW!`);
+        }
+    }
+
+    feed() {
+        this.satiateLevel = 10;
+    }
+    pet() {
+      this.happyLevel++;
+    }
+    exercise() {
+      this.happyLevel++ && this.restLevel++;
+    }
+    happyDown() {
+      setInterval(() => {
+        this.happyLevel--;
+     }, 1000);
+    }
+
+    cries(){
+        if(this.happyLevel > 0) {
+            return false;
+        } else{
+            return console.log("Waahhhhhhh");
+         }
+     }
+       jumpRope() {
+        this.happyLevel ++;
+    }
+
+        getSleepy() {
+            setInterval(() => {
+                this.sleepLevel--;
+            }, 1000);
+
+        }
+        sleep(){
+            this.restLevel = 20;
+        }
+    }
